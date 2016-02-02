2c3675f50d770909547b4386e7fd076fdc38dee6
rm0426 <rm0426@gmail.com>
2016-02-03 01:08:36 +0900

    親のActivityを決定

--- app/src/main/java/com/example/rm0426/mynamescoreapp/MyResult.java
+++ app/src/main/java/com/example/rm0426/mynamescoreapp/MyResult.java
@@ -5,7 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MyResult extends AppCompatActivity {
+
+    private static final String STR_SCORE   = "'s score";
+    private static final String STR_PT      = "pt !!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,13 @@ public class MyResult extends AppCompatActivity {
         Intent intent = getIntent();
         String myName = intent.getStringExtra(MyForm.EXTRA_NAME);
         TextView namelabel = (TextView) findViewById(R.id.namelabel);
-        namelabel.setText(myName);
+        namelabel.setText(myName + STR_SCORE);
+
+        Random randomGenerator = new Random();
+        int score = randomGenerator.nextInt(101); //0-100
+
+        TextView scorelabel = (TextView) findViewById(R.id.scorelabel);
+        scorelabel.setText(String.valueOf(score) + STR_PT);
+
     }
 }
